6cf04c6bb7a84d6a4e5bd52bc90ccbce038edf84
comrade-del <pobehi18@hmail.com>
2023-06-18 01:24:29 +0100

    actvmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,13 +68,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'desolate-dusk-93830.herokuapp.com'
+  host = 'stormy-plateau-31164.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'apikey',
+    :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
