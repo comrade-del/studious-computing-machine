9f6d19c8643b9d6e8540c43825e7b5bc58c630fe
comrade-del <pobehi18@hmail.com>
2023-06-19 17:59:36 +0100

    visualize

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,12 @@
 class Form < ApplicationRecord
-  #belongs_to :user
-  #validates :user_id, presence: true
+  # belongs_to :user
+  # validates :user_id, presence: true
   validates :id_number, presence: true, length: { is: 10 }, uniqueness: true
+  before_save :downcase_gender
+
+  private
+
+  def downcase_gender
+    self.gender = gender.downcase if gender.present?
+  end
 end
