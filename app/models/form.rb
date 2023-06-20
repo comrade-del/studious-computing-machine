c2ba2bfad9779a6732698b451a5423da65a1be21
comrade-del <pobehi18@hmail.com>
2023-06-20 16:49:01 +0100

    decorate

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,8 +1,21 @@
 class Form < ApplicationRecord
   # belongs_to :user
   # validates :user_id, presence: true
-  validates :id_number, presence: true, length: { is: 10 }, uniqueness: true
   before_save :downcase_gender
+  validates :id_number, presence: true, length: { is: 10 }, uniqueness: true
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :house_size, presence: true
+  validates :relationship, presence: true
+  validates :marital_status, presence: true
+  validates :religion, presence: true
+  validates :education, presence: true
+  validates :employment, presence: true
+  validates :income_level, presence: true
 
   private
 
